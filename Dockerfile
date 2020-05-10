7a7bcf46af657a9e92521b45c6b5c8f7d78da2a8
james <james@quizlicious.com>
2020-05-10 09:58:32 -0400

    Added pyTenable library and checks for the Tenable.io API keys

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:alpine
 WORKDIR /usr/src/app
 
 COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY tenable-image-counter.py ./
 
--- tenable-image-counter.py
+++ tenable-image-counter.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 
+import os
+from tenable.io import TenableIO
+
 print("Tenable Image Counter")
+TioAccessKey=None
+TioSecretKey=None
+if 'TIO_ACCESS_KEY' in os.environ:
+    TioAccessKey = os.environ['TIO_ACCESS_KEY']
+    print("Tenable.io API key:",TioAccessKey)
+
+if 'TIO_SECRET_KEY' in os.environ:
+    TioSecretKey=os.environ['TIO_SECRET_KEY']
+
+if TioAccessKey is None or TioSecretKey is None:
+    print("Unable to connect to Tenable.io without both the access key and secret key")
